ce4fec5cd62112dd9c08d6a02fa0f25604aa5806
Waleedkhani255 <201253134+WaleedAfridi-1@users.noreply.github.com>
2026-03-20 01:46:08 +0500

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+
+      <div className='w-full flex justify-between '>
+
+      <div className='flex flex-col '>
+        <h1 className='text-3xl lg:text-5xl font-bold'>Home</h1>
+        <p>Snippet</p>
+      </div>
+      <div>
+        <button></button>
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
