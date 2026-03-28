c90bf8c21983b71aeda87e5db18e0d3b899e274a
Waleed Afridi <patlu7150@gmail.com>
2026-03-28 15:47:03 +0500

    Update route.js

--- app/api/snippet/[id]/route.js
+++ app/api/snippet/[id]/route.js
@@ -1,1 +1,24 @@
+import { prisma } from "@/lib/prisma";
 
+export const GET = async (req, { params }) => {
+    try {
+        const id = Number((await params).id);
+
+        const snippet = await prisma.snippet.findUnique({
+            where: { id },
+        });
+
+
+        const all = await prisma.snippet.findMany();
+
+        return Response.json(snippet || null);
+        
+    } catch (error) {
+        console.log("API ERROR:", error);
+
+        return new Response(
+            JSON.stringify({ error: "Something went wrong" }),
+            { status: 500 }
+        );
+    }
+};
